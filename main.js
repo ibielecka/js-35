e8782989c6263ea5bf1c816938f5a437e6bf2f82
ibielecka <iwona.k.michalik@gmail.com>
2023-02-22 09:28:59 +0100

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+function przedzial(n) 
+{
+    if(n >= 100 && n <= 200) {
+        console.log("liczba znajduje sie w przedziale");    
+    }
+    else {
+        console.log("liczba nie znajduje się w przedziale");
+
+    }
+
+}
+przedzial(80);
